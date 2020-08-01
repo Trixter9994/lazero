ce8a14566db5315408736c0b6665e819cdd104ec
root <root@localhost.localdomain>
2020-08-01 06:39:30 -0500

    fuck

---
+++ welcome.py
@@ -0,0 +1,10 @@
+from termcolor import colored
+print()
+print(colored("    -","red"))
+print(colored("   |    ","red")+colored("            ___  __  __","cyan"))
+print(colored("  / \   ","red")+colored("|    /|  /  ___ |   |  |","cyan"))
+print(colored(" \  _\  ","red")+colored("|__ / | /__ ___ |   |__|","cyan"))
+print()
+print(colored("""To make everything
+executable, analyzable, controllable.""","yellow"))
+print()
