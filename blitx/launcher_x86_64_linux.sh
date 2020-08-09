5b026f686528ddb345050c65d85f7e7fa510c07e
root <root@localhost.localdomain>
2020-08-09 15:07:06 -0500

    fuck

--- blitx/launcher_x86_64_linux.sh
+++ blitx/launcher_x86_64_linux.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+file_target=$( ./on_premise_filter.sh )
 cd bin/linux/x86_64
-./libjudger.so --exe_path=$(which bash) --seccomp_rule_name="randomfuck" --max_cpu_time=500 --max_real_time=5000 --max_memory=14213120 --max_stack=500000 --max_process_number=2 --args="--rcfile" --args="../../../bashrc"
+#./libjudger.so --exe_path=$(which bash) --seccomp_rule_name="randomfuck" --max_cpu_time=500 --max_real_time=5000 --max_memory=14213120 --max_stack=500000 --max_process_number=2 --args="--rcfile" --args="../../../bashrc"
+# does that work without intervention?
+sudo echo $$ > $file_target && sudo ./libjudger.so --exe_path=$(which bash) --seccomp_rule_name="randomfuck"  --max_cpu_time=500 --max_real_time=10000 --max_memory=14213120 --max_stack=500000 --max_process_number=2  
