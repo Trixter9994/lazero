cb5870d09fc297db4fc5135b4be56261cbea509b
root <root@localhost.localdomain>
2020-08-20 07:10:34 -0500

    fuck

--- blitx/src/audio_record/sc.py
+++ blitx/src/audio_record/sc.py
@@ -4,21 +4,24 @@ import numpy
 # you can use numba. just maybe.
 # but useless on ARM.
 # get a list of all speakers:
+# this is shit.
 speakers = sc.all_speakers()
 # get the current default speaker on your system:
 default_speaker = sc.default_speaker()
 # get a list of all microphones:
-mics = sc.all_microphones()
+mics = sc.all_microphones(include_loopback=True,exclude_monitors=False)
+print(mics)
 # get the current default microphone on your system:
-default_mic = sc.default_microphone()
+#default_mic = sc.default_microphone()
+default_mic=mics[0]
 
-#data = default_mic.record(samplerate=48000, numframes=48000*5)
+data = default_mic.record(samplerate=48000, numframes=48000*5)
 # reshape the thing?
-data=numpy.linspace(-1000,10000,48000*5)
+"""data=numpy.linspace(-1000,10000,48000*5)
 data=numpy.sin(data)
 data=data.tolist()
 data=list(zip(data,data))
-data=numpy.array(data)
+data=numpy.array(data)"""
 print("finished record")
 # what the fuck?
 # so it makes the sound.
