1c3f7a42b3da568172118f03e256784be5f4fd2a
fuck <fuck@github.com>
2020-08-24 19:37:33 +0800

    fuck

--- keller/src/ralink.py
+++ keller/src/ralink.py
@@ -1,5 +1,5 @@
 import redis   # 导入redis 模块
-
+from itertools import zip_longest as izip_longest
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
 def rset(a,b,c=3):
@@ -25,3 +25,16 @@ def rbset(a,b,c=3):
         pipe.setex(x,c,b)
     pipe.execute()
     print("done: ",len(a))
+
+def batcher(iterable, n):
+    args = [iter(iterable)] * n
+    return izip_longest(*args)
+
+# in batches of 500 delete keys matching user:*
+def rka():
+    return batcher(r.scan_iter('*'),500)
+
+def pka():
+    for keybatch in rka():
+        print(*keybatch)
+
