f6bde30e7a765b99a12c9f7e58068a8e8c67b04c
fuck <fuck@github.com>
2020-08-24 13:30:22 +0800

    fuck

--- keller/src/gobuild.sh
+++ keller/src/gobuild.sh
@@ -5,3 +5,4 @@ cd go
 mkdir bin src pkg
 GO111MODULE=on GOPATH=$(pwd) go get github.com/buildkite/terminal-to-html
 GO111MODULE=on GOPATH=$(pwd) go get github.com/codegangsta/cli
+GO111MODULE=on GOPATH=$(pwd) go get github.com/urfave/cli
