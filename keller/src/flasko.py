a2859a7d5c7b58d569ce5d0962ce2ef5a500f058
fuck <fuck@fuck.com>
2020-09-04 22:25:42 +0800

    fuck

--- keller/src/flasko.py
+++ keller/src/flasko.py
@@ -14,6 +14,7 @@ import sys
 import os
 from flask_sockets import Sockets
 import time
+#from threading import Thread
 from gevent import monkey
 from flask import Flask, request
 from gevent import pywsgi
@@ -32,7 +33,7 @@ now = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime(time.time()))
 def echo_socket(ws,path):
     while not ws.closed:
         print("WS: ",path)
-        ws.send(str("message test!"))  # 回传给clicent
+        ws.send("1+250")  # 回传给clicent
         """ 服务端必须接收到客户端发的消息才能保持该服务运行，如果ws.receive()没有接收到客户端发送的
          消息，那么它会关闭与客户端建立的链接
          底层解释：Read and return a message from the stream. If `None` is returned, then
@@ -42,8 +43,12 @@ def echo_socket(ws,path):
         message = ws.receive()  # 接收到消息
         if message is not None:
             print("%s receive msg==> " % now, str(json.dumps(message)))
+            def sendmsg():
+                time.sleep(0.5)
+                ws.send(str(json.dumps(message)))  # 回传给clicent
+            sendmsg()
+            #Thread(target=sendmsg,args=()).start()
             """ 如果客户端未发送消息给服务端，就调用接收消息方法，则会导致receive()接收消息为空，关闭此次连接 """
-            ws.send(str(json.dumps(message)))  # 回传给clicent
         else:
             print(now, "no receive")
  
